da1e50b6d6a079408f168f41ddd4ed99765a8927
alanvf1 <alan.varela404@gmail.com>
2024-11-03 17:25:29 -0300

    lint fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class">
-          <div className="min-h-screen lg:grid lg:grid-cols-app dark:bg-zinc-900">
+          <div className="min-h-screen dark:bg-zinc-900 lg:grid lg:grid-cols-app">
             <Sidebar />
             <main className="h-full min-h-screen max-w-[100vw] px-4 pb-12 pt-24 lg:col-start-2 lg:px-8 lg:pb-12 lg:pt-8">
               {children}
